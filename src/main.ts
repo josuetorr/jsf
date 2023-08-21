c853b903877d32f14d5b5b4f1881be88a2fdac08
Josue Torres <josue.dtorres@gmail.com>
2023-08-21 00:43:33 -0400

    init

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,17 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+function tag(
+  name: keyof HTMLElementTagNameMap,
+  ...children: Array<HTMLElement | string>
+) {
+  const el = document.createElement(name);
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+  for (const c of children) {
+    if (typeof c === 'string') el.appendChild(document.createTextNode(c));
+    else el.appendChild(c);
+  }
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  return el;
+}
+
+const app = tag('div', 'test test test');
+
+document.querySelector('#app')?.appendChild(app);
